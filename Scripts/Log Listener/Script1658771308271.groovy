b2a81ac710c2065e10ff7acb265b863c0a8cb4b1
Coty Rosenblath <coty@pobox.com>
2022-07-25 14:07:09 -0400

    Fix Log Listener example
    Somehow we lost the script.

---
+++ Scripts/Log Listener/Script1658771308271.groovy
@@ -0,0 +1,59 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import org.openqa.selenium.WebDriver as WebDriver
+import org.openqa.selenium.chrome.ChromeDriver as ChromeDriver
+import org.openqa.selenium.WebElement as WebElement
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
+import org.openqa.selenium.By as By
+import org.openqa.selenium.support.locators.RelativeLocator as RelativeLocator
+import org.openqa.selenium.support.locators.RelativeLocator.RelativeBy as RelativeBy
+import static org.openqa.selenium.support.locators.RelativeLocator.with
+import org.openqa.selenium.WindowType as WindowType
+import org.openqa.selenium.devtools.DevTools as DevTools
+import org.openqa.selenium.devtools.v101.log.Log as Log
+
+System.setProperty('webdriver.chrome.driver', DriverFactory.getChromeDriverPath())
+
+ChromeDriver driver = new ChromeDriver()
+
+println(driver)
+
+DevTools devTools = driver.getDevTools()
+
+devTools.createSession()
+
+// Enable The Console Logs
+devTools.send(Log.enable())
+
+// Add A Listener For The Logs
+devTools.addListener(Log.entryAdded(), { def logEntry ->
+        System.out.println('----------')
+
+        System.out.println('Level: ' + logEntry.getLevel())
+
+        System.out.println('Text: ' + logEntry.getText())
+
+        System.out.println('Broken URL: ' + logEntry.getUrl())
+    })
+
+// Load The AUT
+driver.get('http://the-internet.herokuapp.com/broken_images')
+
+driver.close()
+
