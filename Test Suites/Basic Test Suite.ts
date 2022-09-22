07b4e161f82c2302b3969d8bceef5c9da7ac48df
Coty Rosenblath <coty@pobox.com>
2022-09-21 23:05:43 -0400

    Adding basic (non-Selenium 4) test case and suite.

---
+++ Test Suites/Basic Test Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Basic Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>15e6fc32-7b88-42af-9711-73388e084b66</testSuiteGuid>
+   <testCaseLink>
+      <guid>aff6f02a-2ecd-4fdb-bdcf-59ffe28f18c8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Basic Test Case</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
